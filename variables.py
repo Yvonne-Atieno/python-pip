14ba3496eb9f11dfc31b7f0c8275f5168e2f673d
Yvonne-Atieno <oyvonneatieno@gmail.com>
2023-04-13 08:16:20 +0300

    first commit

---
+++ variables.py
@@ -0,0 +1,21 @@
+# create list and print value availiable at index 2
+list=['pysics','chemistry','maths','apple'];
+print (list[2])
+
+# create variables and return sum
+
+def sum(x,y):
+ x+y
+ return sum
+
+# Declare an integer variable
+b=4
+print(b)
+
+# create string in varible
+x="Hello"
+print(x)
+
+# Given the string "Hello Python World"print the first character of each word using positive indexing
+c="Hello python world"
+print(c[0:5:11])
